411b8c856ef4d5becd015dc14d006a364a0847ef
MCJack123 <jackmacwindowslinux@gmail.com>
2026-07-21 22:08:14 -0400

    Fixed scheme, updated a few things

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import L4RCApiData from "./ApiData"
 import { L4RCAutocomplete } from "./Autocomplete"
 import { L4RCHover } from "./Hover"
 
-const LUA_MODE = { language: "lua", scheme: "file" }
+const LUA_MODE = { language: "lua" }
 
 export function activate(context: vscode.ExtensionContext) {
     let dataPath = context.asAbsolutePath("./data")
